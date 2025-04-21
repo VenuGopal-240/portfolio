610e4f4a5a9ab73cf312eeaa7b04f18e465ea624
VenuGopal-240 <sanaboinavenugopal@gmail.com>
2025-04-21 10:42:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './ResumeTemplate/Mainpage';
-import { Bro } from './ResumeTemplate/Template/Example';
-import Center from './ResumeTemplate/Template/Bro';
-import ParentCompOfFunctionCalling from './NestedChild/ParentCompOfFunctionCalling';
-import OTPGenerator from './OTP/OtpGenerator';
-import Form from './CRUD/FormData';
-import Counter from './Redux/Counter';
-import { Provider } from 'react-redux';
-import { store } from './Redux/Store';
-import UseReducerCounter from './UseReducer/UseReducerCounter';
-import TimeSheet from './TE/TimeSheet';
-import CurrentWeekView from './TE/Exxample'
-import ButtonToggleExample from './TE/Exxample';
-import MyComponent from './TE/Exxample';
 
 function App() {
   return (
     <>
       <MainPage />
-      {/* <Bro/> */}
-      {/* <Center/> */}
-      {/* <ParentCompOfFunctionCalling/> */}
-      {/* <OTPGenerator/> */}
-      {/* <Form/> */}
-      {/* <Provider store={store}>
-        <Counter />
-      </Provider> */}
-
-      {/* <UseReducerCounter/> */}
-      {/* <TimeSheet/> */}
-      {/* <CurrentWeekView/> */}
-      {/* <ButtonToggleExample/> */}
-      {/* <MyComponent/> */}
     </>
   );
 }
