31a56d56cc97115c75b782835821913d9d096ebf
VenuGopal-240 <sanaboinavenugopal@gmail.com>
2025-04-21 10:40:50 +0530

    Delete src/Redux directory

--- src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-// src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../Redux/counterSlics';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
